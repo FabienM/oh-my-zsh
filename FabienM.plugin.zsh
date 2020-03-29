4e2b99d7a05d37de3c07ccd567bf16b8701c8b83
Fabien Meurillon <fabien.meurillon@decathlon.com>
2020-03-29 19:10:47 +0200

    Add plouf encoder

--- FabienM.plugin.zsh
+++ FabienM.plugin.zsh
@@ -4,5 +4,21 @@
 alias cat=bat
 alias ls=exa
 alias ping=prettyping
+alias pwgen="pwgen -cy 32 1"
+alias pass=gopass
 
 function gi() { curl -sLw "\n" https://www.gitignore.io/api/\$@ ;}
+function wttr() { curl -4 wttr.in/${1:-Lille} }
+
+function plouf() {
+	PASS="$(pwgen)"
+	ENC="$(openssl enc -aes-256-cbc -salt -a -pass pass:$PASS -in $1)"
+	echo "Password: $PASS"
+	unplouf $ENC
+}
+
+function unplouf() {
+	UNPLOUF="openssl enc -aes-256-cbc -d -a << EOF\n$1\nEOF"
+	echo $UNPLOUF
+	echo "$UNPLOUF" | pbcopy
+}
